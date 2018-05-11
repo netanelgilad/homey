c3dc717a5bc4cc10d96627b3fff9d690abf8acdb
Netanel Gilad <netanel@botique.ai>
2018-05-11 20:07:06 +0300

    not const

--- tv-shows.js
+++ tv-shows.js
@@ -27,6 +27,9 @@ exports.tvShows = function(app) {
             tvShow = tvShow.replace("of ", "");
         }
 
+        tvShow.replace("'", "");
+        tvShow.replace(/ \w /, " ");
+
         const showInfo = await safeGet(`http://api.tvmaze.com/singlesearch/shows?q=${tvShow}`);
         const lastEpisodeInfo = await safeGet(showInfo._links.previousepisode.href);
         const season = lastEpisodeInfo.season;
