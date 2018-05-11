e480106f13f741de3575907c814af74640f9147d
Netanel Gilad <netanel@botique.ai>
2018-05-12 02:44:19 +0300

    try fix subs

--- tv-shows.js
+++ tv-shows.js
@@ -38,6 +38,15 @@ const safeGet = async url => {
 exports.tvShows = function(app) {
     app.post('/tv-show', async (req, res) => {
         let tvShow = req.body.tvShow;
+
+        if (tvShow.startsWith("of ")) {
+            tvShow = tvShow.replace("of ", "");
+        }
+
+        tvShow = tvShow.replace("'", "");
+        tvShow = tvShow.replace(/ \w /, " ");
+        tvShow = tvShow.replace("-", " ");
+
         console.log("got request to stream tv show", tvShow);
         let season = req.body.season;
         let episode = req.body.episode;
@@ -53,14 +62,6 @@ exports.tvShows = function(app) {
             season = Math.floor(episode/100);
             episode = episode % 100;
         }
-
-        if (tvShow.startsWith("of ")) {
-            tvShow = tvShow.replace("of ", "");
-        }
-
-        tvShow = tvShow.replace("'", "");
-        tvShow = tvShow.replace(/ \w /, " ");
-        tvShow = tvShow.replace("-", " ");
         
         console.log(`Downloading torrent for season ${season} and episode ${episode}`);
 
