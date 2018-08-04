382b986e6d65943b0dbc6473f3a22a19fa8a6679
Netanel Gilad <netanel@botique.ai>
2018-08-04 23:00:29 +0300

    fix for not using canonized name

--- packages/server/src/tv-shows/tv-show-functions.ts
+++ packages/server/src/tv-shows/tv-show-functions.ts
@@ -38,7 +38,7 @@ export async function downloadTVShowEpisode(
     torrent: await ensureTorrentForEpisode(
       client,
       downloadedTVShowsCollection,
-      tvShow,
+      tvShowName,
       foundSeason,
       foundEpisode
     ),
