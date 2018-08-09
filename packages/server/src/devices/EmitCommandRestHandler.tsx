1b5626f6b90b2928ed7278542a889d38c73e71e4
Netanel Gilad <netanel@botique.ai>
2018-08-09 11:56:18 +0300

    log on commands

--- packages/server/src/devices/EmitCommandRestHandler.tsx
+++ packages/server/src/devices/EmitCommandRestHandler.tsx
@@ -1,24 +1,23 @@
-import { RestActionHandler } from '../rest-actions/RestActionHandler';
-import React = require('react');
-import { emitCommandRestAction } from './emitCommandRestAction';
-import { Device } from './Device';
-import { AllDeviceCommands } from './AllDeviceCommands';
-import { emitCommand } from './runCommand';
+import { RestActionHandler } from "../rest-actions/RestActionHandler";
+import React = require("react");
+import { emitCommandRestAction } from "./emitCommandRestAction";
+import { Device } from "./Device";
+import { AllDeviceCommands } from "./AllDeviceCommands";
+import { emitCommand } from "./runCommand";
 
-export function EmitCommandRestHandler(props: {
-    activeDevice: Device;
-}) {
-    return (
-        <RestActionHandler
-            restAction={emitCommandRestAction}
-            handler={({name}) => {
-                const deviceCommand = AllDeviceCommands.get(name);
-                if (!deviceCommand) {
-                    throw new Error(`No device command found for ${name}`);
-                }
+export function EmitCommandRestHandler(props: { activeDevice: Device }) {
+  return (
+    <RestActionHandler
+      restAction={emitCommandRestAction}
+      handler={({ name }) => {
+        const deviceCommand = AllDeviceCommands.get(name);
+        if (!deviceCommand) {
+          throw new Error(`No device command found for ${name}`);
+        }
 
-                emitCommand(props.activeDevice, deviceCommand);
-            }}
-        />
-    )
+        console.log("emitting command ", name);
+        emitCommand(props.activeDevice, deviceCommand);
+      }}
+    />
+  );
 }
