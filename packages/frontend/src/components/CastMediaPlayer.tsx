cf394a79356aca17d474d98c85b06dae459e891c
Netanel Gilad <netanel@botique.ai>
2018-08-10 23:36:12 +0300

    handle IDLE state

--- packages/frontend/src/components/CastMediaPlayer.tsx
+++ packages/frontend/src/components/CastMediaPlayer.tsx
@@ -5,7 +5,7 @@ export class CastMediaPlayer extends React.Component<
   {
     onVideoPlaying();
     onVideoStopped();
-    onDisplayMessage(event: {type: "error" | "info", message: string});
+    onDisplayMessage(event: { type: "error" | "info"; message: string });
   },
   {
     failedLoadingPlayer: boolean;
@@ -13,63 +13,65 @@ export class CastMediaPlayer extends React.Component<
 > {
   private ref: React.RefObject<HTMLDivElement>;
 
-  constructor(
-    props
-  ) {
+  constructor(props) {
     super(props);
 
     this.ref = React.createRef();
     this.state = {
-        failedLoadingPlayer: false
+      failedLoadingPlayer: false
     };
   }
 
   public render() {
-      if (!this.state.failedLoadingPlayer) {
-        return (
-            <div style={{ position: "relative", height: "100%" }} ref={this.ref} />
-          );
-      }
-      else {
-          return (
-            <div style={{ position: "relative", height: "100%", background: "black" }}>
-                <h5 style={{color: "white"}}>This is where the player appears</h5>
-            </div>
-          )
-      }
+    if (!this.state.failedLoadingPlayer) {
+      return (
+        <div style={{ position: "relative", height: "100%" }} ref={this.ref} />
+      );
+    } else {
+      return (
+        <div
+          style={{ position: "relative", height: "100%", background: "black" }}
+        >
+          <h5 style={{ color: "white" }}>This is where the player appears</h5>
+        </div>
+      );
+    }
   }
 
   public componentDidMount() {
     this.ref.current!.innerHTML = "<cast-media-player></cast-media-player>";
     try {
-        const context = cast.framework.CastReceiverContext.getInstance();
-        const playerManager = cast.framework.CastReceiverContext.getInstance().getPlayerManager();
-        let playing = false;
-    
-        playerManager.addEventListener(
-          cast.framework.events.EventType.MEDIA_STATUS,
-          (event: any) => {
-            console.log(event);
-            if (!playing && event.mediaStatus.playerState === "PLAYING") {
-              playing = true;
-              this.props.onVideoPlaying();
-            } else if (playing && event.mediaStatus.playerState === "PAUSED") {
-              playing = false;
-              this.props.onVideoStopped();
-            }
+      const context = cast.framework.CastReceiverContext.getInstance();
+      const playerManager = cast.framework.CastReceiverContext.getInstance().getPlayerManager();
+      let playing = false;
+
+      playerManager.addEventListener(
+        cast.framework.events.EventType.MEDIA_STATUS,
+        (event: any) => {
+          console.log(event);
+          if (!playing && event.mediaStatus.playerState === "PLAYING") {
+            playing = true;
+            this.props.onVideoPlaying();
+          } else if (
+            (playing && event.mediaStatus.playerState === "PAUSED") ||
+            event.mediaStatus.playerState === "IDLE"
+          ) {
+            playing = false;
+            this.props.onVideoStopped();
           }
-        );
+        }
+      );
 
-        context.addCustomMessageListener("urn:x-cast:com.homey.messages", (customEvent) => {
+      context.addCustomMessageListener(
+        "urn:x-cast:com.homey.messages",
+        customEvent => {
           this.props.onDisplayMessage(customEvent.data);
-        });
-        
-        context.start();
+        }
+      );
 
+      context.start();
+    } catch (err) {
+      this.setState({ failedLoadingPlayer: true });
     }
-    catch (err) {
-        this.setState({ failedLoadingPlayer : true });
-    }
-    
   }
 }
