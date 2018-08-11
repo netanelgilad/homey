6253d0d7fccce0dff7dfd6f5888f5c74f32028b3
Netanel Gilad <netanel@botique.ai>
2018-08-11 12:00:59 +0300

    refactor ServerStats

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,13 +2,11 @@ import * as React from "react";
 import Layout from "antd/lib/layout";
 import message from "antd/lib/message";
 import "./App.css";
-import { TvShowsList, MegaByte, KiloByte, GigaByte } from "./components/TvShowsList";
+import {
+  TvShowsList} from "./components/TvShowsList";
 import { CastMediaPlayer } from "./components/CastMediaPlayer";
-import { State, Interval } from "@react-atoms/core";
-import Button from "antd/lib/button";
-import { APIAction } from "./network/APIAction";
-import Axios from "../node_modules/axios";
-import Icon from "antd/lib/icon";
+import { State } from "@react-atoms/core";
+import { ServerStats } from "./components/ServerStats";
 
 message.config({
   top: 50
@@ -30,73 +28,7 @@ class App extends React.Component {
               </Layout.Sider>
               <Layout.Content>
                 <div style={{ height: "50px", backgroundColor: "#ffffff" }}>
-                  <APIAction<
-                    {},
-                    {
-                      cpuUsage: number;
-                      freeMemory: number;
-                      deviceDetected: boolean;
-                      chromecastConnected: boolean;
-                      freeSpace: number;
-                    }
-                  >
-                    initialValue={{
-                      cpuUsage: 0,
-                      freeMemory: 0,
-                      freeSpace: 0,
-                      deviceDetected: false,
-                      chromecastConnected: false
-                    }}
-                    activate={() => Axios.get("/server/stats")}
-                  >
-                    {({ value, call }) => (
-                      <>
-                        <Button.Group style={{ float: "right" }}>
-                          <Button style={{ height: "50px", width: "130px" }}>
-                            <Icon
-                              type="chrome"
-                              style={
-                                value!.chromecastConnected
-                                  ? { color: "aqua" }
-                                  : { color: "orange" }
-                              }
-                            />
-                            Chromecast
-                          </Button>
-                          <Button style={{ height: "50px", width: "130px" }}>
-                            {value!.deviceDetected ? (
-                              <Icon
-                                type="check-circle"
-                                style={{ color: "aqua" }}
-                              />
-                            ) : (
-                              <Icon
-                                type="exclamation-circle"
-                                style={{ color: "orange" }}
-                              />
-                            )}
-                            Remote
-                          </Button>
-                          <Button style={{ height: "50px", width: "130px" }}>
-                            <Icon type="dashboard" />
-                            {(value!.cpuUsage * 100).toFixed(2)}%
-                          </Button>
-                          <Button style={{ height: "50px", width: "130px" }}>
-                            <Icon type="database" />
-                            {value!.freeMemory.toFixed(2)}
-                            MB
-                          </Button>
-                          <Button style={{ height: "50px", width: "130px" }}>
-                            <Icon type="file" />
-                            {Space(value!.freeSpace)}
-                          </Button>
-                        </Button.Group>
-                        {!state.isPlaying && (
-                          <Interval interval={1500} run={call} />
-                        )}
-                      </>
-                    )}
-                  </APIAction>
+                  <ServerStats poll={!state.isPlaying} />
                 </div>
                 <div
                   style={
@@ -128,21 +60,6 @@ class App extends React.Component {
       </State>
     );
   }
-}
-
-export function Space(space: number ) {
-  let result = space.toFixed(2) + "B";
-
-  if (space > GigaByte * 1.1) {
-    result = (space / GigaByte).toFixed(2) + "GB";
-  }
-  else if (space > MegaByte * 1.1) {
-    result = (space / MegaByte).toFixed(2) + "MB";
-  } else if (space > KiloByte * 1.1) {
-    result = (space / KiloByte).toFixed(2) + "K";
-  }
-
-  return result;
 }
 
 export default App;
---
+++ packages/frontend/src/components/ServerStats.tsx
@@ -0,0 +1,85 @@
+import * as React from "react";
+import { APIAction } from "../network/APIAction";
+import Axios from "axios";
+import Button from "antd/lib/button";
+import Icon from "antd/lib/icon";
+import { Interval } from "@react-atoms/core";
+import { GigaByte, MegaByte, KiloByte } from "./TvShowsList";
+
+export function ServerStats(props: { poll: boolean }) {
+  return (
+    <APIAction<
+      {},
+      {
+        cpuUsage: number;
+        freeMemory: number;
+        deviceDetected: boolean;
+        chromecastConnected: boolean;
+        freeSpace: number;
+      }
+    >
+      initialValue={{
+        cpuUsage: 0,
+        freeMemory: 0,
+        freeSpace: 0,
+        deviceDetected: false,
+        chromecastConnected: false
+      }}
+      activate={() => Axios.get("/server/stats")}
+    >
+      {({ value, call }) => (
+        <>
+          <Button.Group style={{ float: "right" }}>
+            <Button style={{ height: "50px", width: "130px" }}>
+              <Icon
+                type="chrome"
+                style={
+                  value!.chromecastConnected
+                    ? { color: "aqua" }
+                    : { color: "orange" }
+                }
+              />
+              Chromecast
+            </Button>
+            <Button style={{ height: "50px", width: "130px" }}>
+              {value!.deviceDetected ? (
+                <Icon type="check-circle" style={{ color: "aqua" }} />
+              ) : (
+                <Icon type="exclamation-circle" style={{ color: "orange" }} />
+              )}
+              Remote
+            </Button>
+            <Button style={{ height: "50px", width: "130px" }}>
+              <Icon type="dashboard" />
+              {(value!.cpuUsage * 100).toFixed(2)}%
+            </Button>
+            <Button style={{ height: "50px", width: "130px" }}>
+              <Icon type="database" />
+              {value!.freeMemory.toFixed(2)}
+              MB
+            </Button>
+            <Button style={{ height: "50px", width: "130px" }}>
+              <Icon type="file" />
+              {Space(value!.freeSpace)}
+            </Button>
+          </Button.Group>
+          {!props.poll && <Interval interval={1500} run={call} />}
+        </>
+      )}
+    </APIAction>
+  );
+}
+
+export function Space(space: number) {
+  let result = space.toFixed(2) + "B";
+
+  if (space > GigaByte * 1.1) {
+    result = (space / GigaByte).toFixed(2) + "GB";
+  } else if (space > MegaByte * 1.1) {
+    result = (space / MegaByte).toFixed(2) + "MB";
+  } else if (space > KiloByte * 1.1) {
+    result = (space / KiloByte).toFixed(2) + "K";
+  }
+
+  return result;
+}
